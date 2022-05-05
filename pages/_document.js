303e18da77618256e93cdcb555e25c8fb4e924d9
Kirusanth Poopalasingam <code@kiru.io>
2022-05-05 11:45:58 +0100

    add ad

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,9 @@ export default function Document() {
     <Html>
       <Head>
         <script defer data-domain="debugle.net" src="https://plausible.io/js/plausible.js"></script>
+        <script async
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4146890929235011"
+                crossOrigin="anonymous"></script>
       </Head>
       <body>
       <Main/>
