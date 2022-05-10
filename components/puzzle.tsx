501ff525593d7f63a9ce7a4a6c390013f8dc4845
Kiru <kiru@users.noreply.github.com>
2022-05-10 00:21:49 +0000

    New puzzle via GH action

--- components/puzzle.tsx
+++ components/puzzle.tsx
@@ -1,21 +1,44 @@
 export const solution = {
-  id: 4,
-  filename: "isPalindrome",
+  id: 5,
+  filename: "levenshteinDistance",
   extension: "js",
-  code: `// source https://github.com/trekhleb/javascript-algorithms/tree/master/src/algorithms/string/palindrome
-function isPalindrome(string) {
-  let left = 0;
-  let right = string.length - 1;
+  code: `// source https://github.com/trekhleb/javascript-algorithms/tree/master/src/algorithms/string/levenshtein-distance
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {number}
+ */
+function levenshteinDistance(a, b) {
+  // Create empty edit distance matrix for all possible modifications of
+  // substrings of a to substrings of b.
+  const distanceMatrix = Array(b.length + 1).fill(null).map(() => Array(a.length + 1).fill(null));
 
-  while (left < right) {
-    if (string[left] !== string[right]) {
-      return false;
-    }
-    left += 1;
-    right -= 1;
+  // Fill the first row of the matrix.
+  // If this is first row then we're transforming empty string to a.
+  // In this case the number of transformations equals to size of a substring.
+  for (let i = 0; i <= a.length; i += 1) {
+    distanceMatrix[0][i] = i;
   }
 
-  return true;
+  // Fill the first column of the matrix.
+  // If this is first column then we're transforming empty string to b.
+  // In this case the number of transformations equals to size of b substring.
+  for (let j = 0; j <= b.length; j += 1) {
+    distanceMatrix[j][0] = j;
+  }
+
+  for (let j = 1; j <= b.length; j += 1) {
+    for (let i = 1; i <= a.length; i += 1) {
+      const indicator = a[i - 1] === b[j - 1] ? 0 : 1;
+      distanceMatrix[j][i] = Math.min(
+        distanceMatrix[j][i - 1] + 1, // deletion
+        distanceMatrix[j - 1][i] + 1, // insertion
+        distanceMatrix[j - 1][i - 1] + indicator, // substitution
+      );
+    }
+  }
+
+  return distanceMatrix[b.length][a.length];
 }
 `
 }
