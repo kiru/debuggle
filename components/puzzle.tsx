59e0a7c103a5a381f64711ab98514e5b5efafcd0
Kiru <kiru@users.noreply.github.com>
2022-05-11 00:20:59 +0000

    New puzzle via GH action

--- components/puzzle.tsx
+++ components/puzzle.tsx
@@ -1,44 +1,27 @@
 export const solution = {
-  id: 5,
-  filename: "levenshteinDistance",
+  id: 6,
+  filename: "euclideanAlgorithm",
   extension: "js",
-  code: `// source https://github.com/trekhleb/javascript-algorithms/tree/master/src/algorithms/string/levenshtein-distance
+  code: `// source https://github.com/trekhleb/javascript-algorithms/tree/master/src/algorithms/math/euclidean-algorithm
 /**
- * @param {string} a
- * @param {string} b
+ * Iterative version of Euclidean Algorithm of finding greatest common divisor (GCD).
+ * @param {number} originalA
+ * @param {number} originalB
  * @return {number}
  */
-function levenshteinDistance(a, b) {
-  // Create empty edit distance matrix for all possible modifications of
-  // substrings of a to substrings of b.
-  const distanceMatrix = Array(b.length + 1).fill(null).map(() => Array(a.length + 1).fill(null));
+function euclideanAlgorithm(originalA, originalB) {
+  // Make input numbers positive.
+  let a = Math.abs(originalA);
+  let b = Math.abs(originalB);
 
-  // Fill the first row of the matrix.
-  // If this is first row then we're transforming empty string to a.
-  // In this case the number of transformations equals to size of a substring.
-  for (let i = 0; i <= a.length; i += 1) {
-    distanceMatrix[0][i] = i;
+  // Subtract one number from another until both numbers would become the same.
+  // This will be out GCD. Also quit the loop if one of the numbers is zero.
+  while (a && b && a !== b) {
+    [a, b] = a > b ? [a - b, b] : [a, b - a];
   }
 
-  // Fill the first column of the matrix.
-  // If this is first column then we're transforming empty string to b.
-  // In this case the number of transformations equals to size of b substring.
-  for (let j = 0; j <= b.length; j += 1) {
-    distanceMatrix[j][0] = j;
-  }
-
-  for (let j = 1; j <= b.length; j += 1) {
-    for (let i = 1; i <= a.length; i += 1) {
-      const indicator = a[i - 1] === b[j - 1] ? 0 : 1;
-      distanceMatrix[j][i] = Math.min(
-        distanceMatrix[j][i - 1] + 1, // deletion
-        distanceMatrix[j - 1][i] + 1, // insertion
-        distanceMatrix[j - 1][i - 1] + indicator, // substitution
-      );
-    }
-  }
-
-  return distanceMatrix[b.length][a.length];
+  // Return the number that is not equal to zero since the last subtraction (it will be a GCD).
+  return a || b;
 }
 `
 }
