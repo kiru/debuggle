27a186d22d2c3d284b1f4994a4b6d58ec5708a66
Kiru <kiru@users.noreply.github.com>
2022-05-13 00:25:05 +0000

    New puzzle via GH action

--- components/puzzle.tsx
+++ components/puzzle.tsx
@@ -1,76 +1,50 @@
 export const solution = {
-  id: 7,
-  filename: "countingSort",
+  id: 8,
+  filename: "Stack",
   extension: "js",
-  code: `// source https://github.com/trekhleb/javascript-algorithms
-countingSort(originalArray, smallestElement = undefined, biggestElement = undefined) {
-  // Init biggest and smallest elements in array in order to build number bucket array later.
-  let detectedSmallestElement = smallestElement || 0;
-  let detectedBiggestElement = biggestElement || 0;
-
-  if (smallestElement === undefined || biggestElement === undefined) {
-    originalArray.forEach((element) => {
-      // Visit element.
-      this.callbacks.visitingCallback(element);
-
-      // Detect biggest element.
-      if (this.comparator.greaterThan(element, detectedBiggestElement)) {
-        detectedBiggestElement = element;
-      }
-
-      // Detect smallest element.
-      if (this.comparator.lessThan(element, detectedSmallestElement)) {
-        detectedSmallestElement = element;
-      }
-    });
+  code: `// Class declaration
+class Stack {
+  constructor () {
+    this.stack = []
+    this.top = 0
   }
 
-  // Init buckets array.
-  // This array will hold frequency of each number from originalArray.
-  const buckets = Array(detectedBiggestElement - detectedSmallestElement + 1).fill(0);
-
-  originalArray.forEach((element) => {
-    // Visit element.
-    this.callbacks.visitingCallback(element);
-
-    buckets[element - detectedSmallestElement] += 1;
-  });
-
-  // Add previous frequencies to the current one for each number in bucket
-  // to detect how many numbers less then current one should be standing to
-  // the left of current one.
-  for (let bucketIndex = 1; bucketIndex < buckets.length; bucketIndex += 1) {
-    buckets[bucketIndex] += buckets[bucketIndex - 1];
+  // Adds a value to the end of the Stack
+  push (newValue) {
+    this.stack.push(newValue)
+    this.top += 1
   }
 
-  // Now let's shift frequencies to the right so that they show correct numbers.
-  // I.e. if we won't shift right than the value of buckets[5] will display how many
-  // elements less than 5 should be placed to the left of 5 in sorted array
-  // INCLUDING 5th. After shifting though this number will not include 5th anymore.
-  buckets.pop();
-  buckets.unshift(0);
-
-  // Now let's assemble sorted array.
-  const sortedArray = Array(originalArray.length).fill(null);
-  for (let elementIndex = 0; elementIndex < originalArray.length; elementIndex += 1) {
-    // Get the element that we want to put into correct sorted position.
-    const element = originalArray[elementIndex];
-
-    // Visit element.
-    this.callbacks.visitingCallback(element);
-
-    // Get correct position of this element in sorted array.
-    const elementSortedPosition = buckets[element - detectedSmallestElement];
-
-    // Put element into correct position in sorted array.
-    sortedArray[elementSortedPosition] = element;
-
-    // Increase position of current element in the bucket for future correct placements.
-    buckets[element - detectedSmallestElement] += 1;
+  // Returns and removes the last element of the Stack
+  pop () {
+    if (this.top !== 0) {
+      this.top -= 1
+      return this.stack.pop()
+    }
+    throw new Error('Stack Underflow')
   }
 
-  // Return sorted array.
-  return sortedArray;
-}
-`
+  // Returns the number of elements in the Stack
+  get length () {
+    return this.top
+  }
+
+  // Returns true if stack is empty, false otherwise
+  get isEmpty () {
+    return this.top === 0
+  }
+
+  // Returns the last element without removing it
+  get last () {
+    if (this.top !== 0) {
+      return this.stack[this.stack.length - 1]
+    }
+    return null
+  }
+
+  // Checks if an object is the instance os the Stack class
+  static isStack (el) {
+    return el instanceof Stack
+  }
+}`
 }
