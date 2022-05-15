57353dba336e288d91e1a4484db7e27736d1c6cb
Kiru <kiru@users.noreply.github.com>
2022-05-15 00:28:47 +0000

    New puzzle via GH action

--- components/puzzle.tsx
+++ components/puzzle.tsx
@@ -1,50 +1,137 @@
 export const solution = {
-  id: 9,
-  filename: "Stack",
+  id: 10,
+  filename: "BloomFilter",
   extension: "js",
-  code: `// https://github.com/TheAlgorithms/JavaScript/blob/master/Data-Structures/Stack/StackES6.js
-class Stack {
-  constructor () {
-    this.stack = []
-    this.top = 0
+  code: `// source https://github.com/trekhleb/javascript-algorithms/blob/master/src/data-structures/bloom-filter/BloomFilter.js
+class BloomFilter {
+  /**
+   * @param {number} size - the size of the storage.
+   */
+  constructor(size = 100) {
+    // Bloom filter size directly affects the likelihood of false positives.
+    // The bigger the size the lower the likelihood of false positives.
+    this.size = size;
+    this.storage = this.createStore(size);
   }
 
-  // Adds a value to the end of the Stack
-  push (newValue) {
-    this.stack.push(newValue)
-    this.top += 1
+  /**
+   * @param {string} item
+   */
+  insert(item) {
+    const hashValues = this.getHashValues(item);
+
+    // Set each hashValue index to true.
+    hashValues.forEach((val) => this.storage.setValue(val));
   }
 
-  // Returns and removes the last element of the Stack
-  pop () {
-    if (this.top !== 0) {
-      this.top -= 1
-      return this.stack.pop()
+  /**
+   * @param {string} item
+   * @return {boolean}
+   */
+  mayContain(item) {
+    const hashValues = this.getHashValues(item);
+
+    for (let hashIndex = 0; hashIndex < hashValues.length; hashIndex += 1) {
+      if (!this.storage.getValue(hashValues[hashIndex])) {
+        // We know that the item was definitely not inserted.
+        return false;
+      }
     }
-    throw new Error('Stack Underflow')
+
+    // The item may or may not have been inserted.
+    return true;
   }
 
-  // Returns the number of elements in the Stack
-  get length () {
-    return this.top
-  }
+  /**
+   * Creates the data store for our filter.
+   * We use this method to generate the store in order to
+   * encapsulate the data itself and only provide access
+   * to the necessary methods.
+   *
+   * @param {number} size
+   * @return {Object}
+   */
+  createStore(size) {
+    const storage = [];
 
-  // Returns true if stack is empty, false otherwise
-  get isEmpty () {
-    return this.top === 0
-  }
-
-  // Returns the last element without removing it
-  get last () {
-    if (this.top !== 0) {
-      return this.stack[this.stack.length - 1]
+    // Initialize all indexes to false
+    for (let storageCellIndex = 0; storageCellIndex < size; storageCellIndex += 1) {
+      storage.push(false);
     }
-    return null
+
+    const storageInterface = {
+      getValue(index) {
+        return storage[index];
+      },
+      setValue(index) {
+        storage[index] = true;
+      },
+    };
+
+    return storageInterface;
   }
 
-  // Checks if an object is the instance os the Stack class
-  static isStack (el) {
-    return el instanceof Stack
+  /**
+   * @param {string} item
+   * @return {number}
+   */
+  hash1(item) {
+    let hash = 0;
+
+    for (let charIndex = 0; charIndex < item.length; charIndex += 1) {
+      const char = item.charCodeAt(charIndex);
+      hash = (hash << 5) + hash + char;
+      hash &= hash; // Convert to 32bit integer
+      hash = Math.abs(hash);
+    }
+
+    return hash % this.size;
+  }
+
+  /**
+   * @param {string} item
+   * @return {number}
+   */
+  hash2(item) {
+    let hash = 5381;
+
+    for (let charIndex = 0; charIndex < item.length; charIndex += 1) {
+      const char = item.charCodeAt(charIndex);
+      hash = (hash << 5) + hash + char; /* hash * 33 + c */
+    }
+
+    return Math.abs(hash % this.size);
+  }
+
+  /**
+   * @param {string} item
+   * @return {number}
+   */
+  hash3(item) {
+    let hash = 0;
+
+    for (let charIndex = 0; charIndex < item.length; charIndex += 1) {
+      const char = item.charCodeAt(charIndex);
+      hash = (hash << 5) - hash;
+      hash += char;
+      hash &= hash; // Convert to 32bit integer
+    }
+
+    return Math.abs(hash % this.size);
+  }
+
+  /**
+   * Runs all 3 hash functions on the input and returns an array of results.
+   *
+   * @param {string} item
+   * @return {number[]}
+   */
+  getHashValues(item) {
+    return [
+      this.hash1(item),
+      this.hash2(item),
+      this.hash3(item),
+    ];
   }
 }`
 }
