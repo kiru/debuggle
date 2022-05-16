8ab830e5d2374acf8c1a7eaf92148ae842d326f4
Kiru <kiru@users.noreply.github.com>
2022-05-16 00:22:47 +0000

    New puzzle via GH action

--- components/puzzle.tsx
+++ components/puzzle.tsx
@@ -1,137 +1,38 @@
 export const solution = {
-  id: 10,
-  filename: "BloomFilter",
+  id: 11,
+  filename: "primeFactors",
   extension: "js",
-  code: `// source https://github.com/trekhleb/javascript-algorithms/blob/master/src/data-structures/bloom-filter/BloomFilter.js
-class BloomFilter {
-  /**
-   * @param {number} size - the size of the storage.
-   */
-  constructor(size = 100) {
-    // Bloom filter size directly affects the likelihood of false positives.
-    // The bigger the size the lower the likelihood of false positives.
-    this.size = size;
-    this.storage = this.createStore(size);
-  }
+  code: `// https://github.com/trekhleb/javascript-algorithms/blob/master/src/algorithms/math/prime-factors/primeFactors.jsjj
+/**
+ * Finds prime factors of a number.
+ *
+ * @param {number} n - the number that is going to be split into prime factors.
+ * @returns {number[]} - array of prime factors.
+ */
+export function primeFactors(n) {
+  // Clone n to avoid function arguments override.
+  let nn = n;
 
-  /**
-   * @param {string} item
-   */
-  insert(item) {
-    const hashValues = this.getHashValues(item);
+  // Array that stores the all the prime factors.
+  const factors = [];
 
-    // Set each hashValue index to true.
-    hashValues.forEach((val) => this.storage.setValue(val));
-  }
-
-  /**
-   * @param {string} item
-   * @return {boolean}
-   */
-  mayContain(item) {
-    const hashValues = this.getHashValues(item);
-
-    for (let hashIndex = 0; hashIndex < hashValues.length; hashIndex += 1) {
-      if (!this.storage.getValue(hashValues[hashIndex])) {
-        // We know that the item was definitely not inserted.
-        return false;
-      }
+  // Running the loop till sqrt(n) instead of n to optimise time complexity from O(n) to O(sqrt(n)).
+  for (let factor = 2; factor <= Math.sqrt(nn); factor += 1) {
+    // Check that factor divides n without a reminder.
+    while (nn % factor === 0) {
+      // Overriding the value of n.
+      nn /= factor;
+      // Saving the factor.
+      factors.push(factor);
     }
-
-    // The item may or may not have been inserted.
-    return true;
   }
 
-  /**
-   * Creates the data store for our filter.
-   * We use this method to generate the store in order to
-   * encapsulate the data itself and only provide access
-   * to the necessary methods.
-   *
-   * @param {number} size
-   * @return {Object}
-   */
-  createStore(size) {
-    const storage = [];
-
-    // Initialize all indexes to false
-    for (let storageCellIndex = 0; storageCellIndex < size; storageCellIndex += 1) {
-      storage.push(false);
-    }
-
-    const storageInterface = {
-      getValue(index) {
-        return storage[index];
-      },
-      setValue(index) {
-        storage[index] = true;
-      },
-    };
-
-    return storageInterface;
+  // The ultimate reminder should be a last prime factor,
+  // unless it is not 1 (since 1 is not a prime number).
+  if (nn !== 1) {
+    factors.push(nn);
   }
 
-  /**
-   * @param {string} item
-   * @return {number}
-   */
-  hash1(item) {
-    let hash = 0;
-
-    for (let charIndex = 0; charIndex < item.length; charIndex += 1) {
-      const char = item.charCodeAt(charIndex);
-      hash = (hash << 5) + hash + char;
-      hash &= hash; // Convert to 32bit integer
-      hash = Math.abs(hash);
-    }
-
-    return hash % this.size;
-  }
-
-  /**
-   * @param {string} item
-   * @return {number}
-   */
-  hash2(item) {
-    let hash = 5381;
-
-    for (let charIndex = 0; charIndex < item.length; charIndex += 1) {
-      const char = item.charCodeAt(charIndex);
-      hash = (hash << 5) + hash + char; /* hash * 33 + c */
-    }
-
-    return Math.abs(hash % this.size);
-  }
-
-  /**
-   * @param {string} item
-   * @return {number}
-   */
-  hash3(item) {
-    let hash = 0;
-
-    for (let charIndex = 0; charIndex < item.length; charIndex += 1) {
-      const char = item.charCodeAt(charIndex);
-      hash = (hash << 5) - hash;
-      hash += char;
-      hash &= hash; // Convert to 32bit integer
-    }
-
-    return Math.abs(hash % this.size);
-  }
-
-  /**
-   * Runs all 3 hash functions on the input and returns an array of results.
-   *
-   * @param {string} item
-   * @return {number[]}
-   */
-  getHashValues(item) {
-    return [
-      this.hash1(item),
-      this.hash2(item),
-      this.hash3(item),
-    ];
-  }
+  return factors;
 }`
 }
